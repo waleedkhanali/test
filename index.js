95a32215e7fbc2e1b8959bead2a97fa7461badf9
waleedkhanali <115911811+waleedkhanali@users.noreply.github.com>
2024-01-06 21:54:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use("/",(req,res) => {
-	res.render("hello from waleed khan")
+	res.send("hello from waleed khan")
 })
 app.listen(8000,() => {
 	console.log("i am running")
