5ecff7062b9e9840a3be789378cbcbf7be6f91bb
Waleed <habibking541@gmail.com>
2024-01-06 21:41:13 -0800

    waleed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use("/",(req,res) => {
-	res.json({message:"hello from Express app"})
+	res.render("hello from waleed khan")
 })
 app.listen(8000,() => {
 	console.log("i am running")
